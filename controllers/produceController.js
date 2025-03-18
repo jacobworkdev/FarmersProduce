ff3c516c80d9e13a3a3c197551f1ac2a9351a70a
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-03-17 21:27:13 -0500

    updateProduct implemented and works

--- controllers/produceController.js
+++ controllers/produceController.js
@@ -1,58 +1,91 @@
 const Produce = require('../models/Produce.js')
 const mongoose = require('mongoose')
-exports.createProduce = async (req,res)=>{
-    try{
-        const {name,quantity,unit,price} = req.body
-        const imageUrl = req.file ? req.file.path:""
+exports.createProduce = async (req, res) => {
+    try {
+        const { name, quantity, unit, price } = req.body
+        const imageUrl = req.file ? req.file.path : ""
 
         const produce = await Produce.create({
-            farmer:req.user.id,
-            name,quantity,unit,price,imageUrl
+            farmer: req.user.id,
+            name, quantity, unit, price, imageUrl
         })
         res.status(201).json(produce)
-    }catch(error){
-        res.status(500).json({message:error.message})
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 }
-exports.getAllProduce = async (req,res)=>{
-    const produce = await Produce.find().populate("farmer",'name')
+exports.getAllProduce = async (req, res) => {
+    const produce = await Produce.find().populate("farmer", 'name')
     res.json(produce)
 }
 
-exports.deleteProduce = async(req,res)=>{
-    try{
-        const {id} = req.params
-        const produce=await Produce.findById(id)
+exports.deleteProduce = async (req, res) => {
+    try {
+        const { id } = req.params
+        const produce = await Produce.findById(id)
 
         console.log(id)
-        if(!produce){
+        if (!produce) {
             console.log(produce)
             console.log(mongoose.Types.ObjectId.isValid(id))
-            return res.status(404).json({message:'produce not found'})
+            return res.status(404).json({ message: 'produce not found' })
         }
 
-        if(produce.farmer.toString()!== req.user.id){
-            return res.status(403).json({message:'unauthorized farmer-produce is posted by some other farmer'})
+        if (produce.farmer.toString() !== req.user.id) {
+            return res.status(403).json({ message: 'unauthorized farmer-produce is posted by some other farmer' })
         }
 
         await produce.deleteOne()
-        res.json({message:'produce deleted successfully'})
-    }catch(error){
-        res.status(500).json({message:error.message})
+        res.json({ message: 'produce deleted successfully' })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
         console.log('error in delete produce controller')
     }
 }
 
-exports.getProduceById= async(req,res)=>{
-    try{
-        const {id}= req.params
+exports.getProduceById = async (req, res) => {
+    try {
+        const { id } = req.params
         const produce = await Produce.findById(id)
-        if(!produce){
-            return res.status(404).json({message:'product not found'})
+        if (!produce) {
+            return res.status(404).json({ message: 'product not found' })
         }
         res.status(200).json(produce)
-    }catch(error){
-        res.status(500).json({message:error.message})
+    } catch (error) {
+        res.status(500).json({ message: error.message })
         console.log('error in getProduceById controller')
+    }
+}
+
+exports.updateProduce = async (req, res) => {
+    try {
+        console.log("updateProd run test")
+        const { id } = req.params
+        const { name, quantity, unit, price } = req.body
+        const produce = await Produce.findById(id)
+        if (!produce) {
+            return res.status(404).json({ message: 'product not found' })
+        }
+        console.log(req.user.id)
+        //validate farmer id
+        if (produce.farmer.toString() !== req.user.id) {
+            return res.status(403).json({ message: 'unauthorized farmer' })
+        }
+
+        const imageUrl = req.file ? req.file.path : produce.imageUrl
+
+        produce.name = name || produce.name;
+        produce.quantity = quantity || produce.quantity;
+        produce.unit = unit || produce.unit;
+        produce.price = price || produce.price;
+        produce.imageUrl = imageUrl;
+
+        const updatedProduce= await produce.save()
+        res.status(200).json({message:'update successfull'})
+
+
+    } catch (error) {
+        res.status(500).json({message:error.message})
+        console.log('error in the produpdate controller')
     }
 }
--- routes/produceRoutes.js
+++ routes/produceRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const {protect} = require('../middleware/authMiddleware.js')
 const upload = require('../middleware/uploadMiddleware.js')
-const {createProduce,getAllProduce,deleteProduce,getProduceById} = require('../controllers/produceController.js')
+const {createProduce,getAllProduce,deleteProduce,getProduceById, updateProduce} = require('../controllers/produceController.js')
 
 const router = express.Router();
 
 router.post('/',protect,upload.single('image'),createProduce)
 router.get('/',getAllProduce)
 router.get('/:id',getProduceById)
+router.put('/:id',protect,upload.single('image'),updateProduce)
 router.delete('/:id',protect,deleteProduce)
 module.exports=router
