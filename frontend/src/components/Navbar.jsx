6e4efa85ee145bb44a5f060be925d75413d1ea75
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-03-20 00:30:42 -0500

    Basic Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+const { default: AuthContext } = require("../context/AuthContext")
+
+
+const Navbar = () =>{
+    const{user,logout} = useContext(AuthContext)
+
+
+        //   <Route path='/' element={<Home />} />
+        //   <Route path='/create-produce' element={<CreateProduce />} />
+        //   <Route path='/my-Produce' element={< Myproduce />} />
+        //   <Route path='/edit-produce/:id' element={<EditProduce />} />
+        //   <Route path='/login' element={< Login />} />
+        //   <Route path='/register' element={<Register />} />
+
+
+    return(
+        <nav>
+            <Link to='/'>Home</Link>
+            {user?(
+                <>
+                <Link to='/create-produce'>Create Produce</Link>
+                <Link to='/my-produce'>My produce</Link>
+                <button onClick={logout}>Logout</button>
+                </>
+            ):(
+                <>
+                <Link to='/login'>Login</Link>
+                <Link to='/register'>Register</Link>
+                </>
+
+            )}
+
+        </nav>
+    )
+}
+
+export default Navbar
