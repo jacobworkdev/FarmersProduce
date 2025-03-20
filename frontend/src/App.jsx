385e2ef37a9ff42c19c69215b88cec5e55226e56
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-03-20 00:04:33 -0500

    added route definitions in the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <Router>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element ={<Home/>}/>
+          <Route path='/create-produce' element ={<CreateProduce />}/>
+          <Route path='/my-Produce' element ={< Myproduce/>}/>
+          <Route path='/edit-produce/:id' element ={<EditProduce />}/>
+          <Route path='/login' element ={< Login/>}/>
+          <Route path='/register' element ={<Register />}/>
+        </Routes>
 
-      
-
-     </Router>
+      </Router>
     </>
   )
 }
