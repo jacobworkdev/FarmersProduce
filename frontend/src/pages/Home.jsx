6d1156238bfa1862fc74606a4ef7a95c5848a5f4
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-03-20 01:23:49 -0500

    home page and fetching is working

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+
+const Home = () => {
+    const [produce, setProduce] = useState([])
+    useEffect(() => {
+        const fetchProduce = async () => {
+            try {
+                const res = await axios.get('http://localhost:5000/api/produce')
+                console.log(res.data)
+                setProduce(res.data)
+            } catch (error) {
+                console.log('error fetching the produce:', error)
+            }
+        }
+        fetchProduce()
+    }, [])
+
+    return (
+        <div className="AllProduceHome">
+            <h1>Hello this is Farmers Produce marketplace!</h1>
+            {produce.length === 0 ? <p>No Produce available right now, Sorry!.</p> : (
+                <div className="AllProduceCardContainer">
+                    {produce.map((item) => (
+                        <div key={item._id} className="ProduceCard">
+                            <h3>{item.name}</h3>
+                            <p>Quantity:{item.quantity} {item.unit}</p>
+                            <p>Price: ${item.price}</p>
+                            <p>Farm - {item.farmer.name}</p>
+                            {item.imageUrl && <img src={item.imageUrl} alt={item.name} width="100" />}
+                        </div>
+                    ))}
+                </div>
+            )}
 
 
-const Home =() =>{
-    return(
-        <h2>this is home buddy</h2>
+        </div>
     )
 }
 
