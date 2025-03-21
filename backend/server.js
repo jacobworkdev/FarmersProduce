7698669b27f3b189d095440719120d1f9a7b26d7
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-03-21 15:17:22 -0500

    removed the port field from .env in the backend folder. to render it on the render.com and made a fail safe check, if process.env.PORT is empty it automatically assigns 5000

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const connectDB = require('./config/db.js')
 const authRoutes= require('./routes/authRoutes.js')
 const produceRoutes = require('./routes/produceRoutes.js')
 
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 5000
 
 dotenv.config()
 //connect to db
@@ -21,7 +21,7 @@ app.use('/api/auth',authRoutes)
 app.use('/api/produce',produceRoutes)
 
 app.listen(PORT,()=>{
-    console.log("server is running on port 5000")
+    console.log(`server is running on port ${PORT}`)
 })
 
 //    my produce route  /api/produce/myproduce
